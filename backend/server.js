62d17c4b178a1cf49f9eeab6b1dbf6a5beeaf7eb
IvanHell <165103464+IvanHell@users.noreply.github.com>
2025-10-28 17:40:09 -0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const db = require('./database');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001; //cambio de 3000 a 3001
 
 // Middleware
 app.use(cors());
